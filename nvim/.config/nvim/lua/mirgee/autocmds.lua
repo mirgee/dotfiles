73ed26e7dbf42c85babd5939f5dd9633f67294cb
Miroslav Kovar <miroslavkovar@protonmail.com>
2023-12-12 06:42:47 +0100

    Highlight yanked text
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- nvim/.config/nvim/lua/mirgee/autocmds.lua
+++ nvim/.config/nvim/lua/mirgee/autocmds.lua
@@ -73,3 +73,11 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.autoindent = true
     end,
 })
+
+-- highlight yanked text
+vim.cmd[[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+augroup END
+]]
