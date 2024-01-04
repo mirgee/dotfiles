f5fbbaa927efc23f2fc3a76b53ff704928c4efb5
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-01-04 20:00:54 +0100

    Change telescope shortcut
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- nvim/.config/nvim/lua/mirgee/configs/telescope.lua
+++ nvim/.config/nvim/lua/mirgee/configs/telescope.lua
@@ -30,6 +30,6 @@ require('telescope').setup{
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>rg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
