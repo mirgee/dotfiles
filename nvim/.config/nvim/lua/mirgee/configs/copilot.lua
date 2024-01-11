73677b79abcee2762bc8b78283488a38c12c0c6d
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-01-11 09:00:22 +0100

    Disable copilot by default
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- nvim/.config/nvim/lua/mirgee/configs/copilot.lua
+++ nvim/.config/nvim/lua/mirgee/configs/copilot.lua
@@ -2,3 +2,4 @@
 vim.api.nvim_set_keymap('i', '<C-E>', 'copilot#Accept("<End>")', { silent = true, script = true, expr = true })
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
+vim.g.copilot_enabled = false
