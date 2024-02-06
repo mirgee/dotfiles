e510fe02c3b91c5ffaa4951b932d5b61603b370d
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-02-06 08:41:07 +0100

    Remove some useless custom snippets and snippets conflicting with friendly snippets
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- nvim/.config/nvim/lua/mirgee/snippets/rust.lua
+++ nvim/.config/nvim/lua/mirgee/snippets/rust.lua
@@ -4,9 +4,7 @@ local snippet_collection = require "luasnip.session.snippet_collection"
 local s = ls.s
 local i = ls.insert_node
 local t = ls.text_node
--- local d = ls.dynamic_node
 local c = ls.choice_node
--- local f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 local same = function(index)
@@ -18,65 +16,6 @@ end
 -- ls.cl
 snippet_collection.clear_snippets "rust"
 ls.add_snippets("rust", {
-  s(
-    "tmain",
-    fmt(
-      [[
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {{
-  Ok(())
-}}]],
-      {}
-    )
-  ),
-
-  s(
-    "yew_comp",
-    fmt(
-      [[use yew::prelude::*;
-
-#[derive(Clone, PartialEq, Properties)]
-pub struct {}Props {{}}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum {}Msg {{}}
-
-pub struct {} {{}}
-
-impl Component for {} {{
-  type Message = {}Msg;
-  type Properties = {}Props;
-
-  fn create(ctx: &Context<Self>) -> Self {{
-      Self {{}}
-  }}
-
-  fn view(&self, ctx: &Context<Self>) -> Html {{
-    todo!()
-  }}
-}}
-]],
-      {
-        same(1),
-        same(1),
-        same(1),
-        i(1),
-        same(1),
-        same(1),
-      }
-    )
-  ),
-
-  s(
-    "main",
-    fmt(
-      [[
-    fn main() {{
-    }}
-    ]],
-      {}
-    )
-  ),
   s(
     "modtest",
     fmt(
@@ -91,6 +30,7 @@ impl Component for {} {{
       i(0)
     )
   ),
+
   s(
     { trig = "test" },
     fmt(
@@ -111,27 +51,6 @@ impl Component for {} {{
       }
     )
   ),
-  s("eq", fmt("assert_eq!({}, {});{}", { i(1), i(2), i(0) })),
-  s("enum", {
-    t { "#[derive(Debug, PartialEq)]", "enum " },
-    i(1, "Name"),
-    t { " {", "  " },
-    i(0),
-    t { "", "}" },
-  }),
-
-  s("struct", {
-    t { "#[derive(Debug, PartialEq)]", "struct " },
-    i(1, "Name"),
-    t { " {", "    " },
-    i(0),
-    t { "", "}" },
-  }),
 
   s("pd", fmt([[println!("{}: {{:?}}", {});]], { same(1), i(1) })),
-  -- _pd = {
-  --   t [[println!("{:?}", ]],
-  --   i(1),
-  --   t [[);]],
-  -- },
 })
