07bfef0cf8f131d251c2e0a3c5ad76e7ad94ce53
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-02-12 19:32:05 +0100

    Remap snippets shortcuts
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- nvim/.config/nvim/lua/mirgee/configs/snippets.lua
+++ nvim/.config/nvim/lua/mirgee/configs/snippets.lua
@@ -18,11 +18,12 @@ ls.config.set_config {
   },
 }
 
-vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+-- vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-S>l", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-S>j", function() ls.jump(-1) end, {silent = true})
 
-vim.keymap.set({"i", "s"}, "<C-S>", function()
+
+vim.keymap.set({"i", "s"}, "<C-S>s", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
 	end
