9e27c02836a82c00d00aa66b6248e0776bcbf3ab
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-02-24 08:44:55 +0100

    Setup harpoon
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

---
+++ nvim/.config/nvim/lua/mirgee/configs/harpoon.lua
@@ -0,0 +1,86 @@
+require("harpoon").setup({
+  global_settings = {
+    -- sets the marks upon calling `toggle` on the ui, instead of require `:w`.
+    save_on_toggle = false,
+
+    -- saves the harpoon file upon every change. disabling is unrecommended.
+    save_on_change = true,
+
+    -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
+    enter_on_sendcmd = false,
+
+    -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
+    tmux_autoclose_windows = false,
+
+    -- filetypes that you want to prevent from adding to the harpoon list menu.
+    excluded_filetypes = { "harpoon" },
+
+    -- set marks specific to each git branch inside git repository
+    mark_branch = false,
+
+    -- enable tabline with harpoon marks
+    tabline = false,
+    tabline_prefix = "   ",
+    tabline_suffix = "   ",
+  }
+})
+
+require("telescope").load_extension('harpoon')
+
+local hui = require('harpoon.ui')
+local hcmdui = require('harpoon.cmd-ui')
+local hm = require('harpoon.mark')
+local ht = require('harpoon.term')
+
+vim.keymap.set('n', '<leader>hm', hm.add_file, { desc = 'Harpoon: Mark File' })
+vim.keymap.set('n', '<leader>he', hui.toggle_quick_menu, { desc = 'Harpoon: Toggle Menu' })
+vim.keymap.set('n', '<leader>hc', hcmdui.toggle_quick_menu, { desc = 'Harpoon: Commands Menu' })
+
+for i = 1, 9 do
+  vim.keymap.set('n', '<leader>h' .. i, function() hui.nav_file(i) end,
+    { desc = 'Harpoon: Go to Mark ' .. i })
+end
+
+for i = 1, 9 do
+  vim.keymap.set('n', '<leader>t' .. i, function() ht.gotoTerminal(i) end,
+    { desc = 'Harpoon: Go to Terminal ' .. i })
+end
+
+for i = 1, 9 do
+  for j = 1, 9 do
+    vim.keymap.set('n', '<leader>c' .. i .. j, function()
+        ht.sendCommand(i, j)
+        ht.sendCommand(i, '\n')
+      end,
+      { desc = 'Harpoon: Send Command ' .. i .. ' to Terminal ' .. j })
+  end
+end
+
+vim.api.nvim_create_user_command(
+  'Ht',
+  function(input)
+    local args = vim.split(input.args, " ", { trimempty = true })
+    if #args < 2 then
+      print("Usage: :Ht <terminal_number> <command>")
+      return
+    end
+
+    local term_num = tonumber(args[1])
+    if not term_num then
+      print("Invalid terminal number")
+      return
+    end
+
+    local command_index = tonumber(args[2])
+    if command_index then
+      ht.sendCommand(term_num, command_index)
+    else
+      table.remove(args, 1)
+      local command = table.concat(args, " ")
+      ht.sendCommand(term_num, command)
+    end
+
+    ht.sendCommand(term_num, '\n')
+  end,
+  { nargs = '*', desc = 'Send command to Harpoon terminal' }
+)
--- nvim/.config/nvim/lua/mirgee/configs/telescope.lua
+++ nvim/.config/nvim/lua/mirgee/configs/telescope.lua
@@ -1,47 +1,47 @@
 require("telescope").setup({
-	defaults = {
-		-- Default configuration for telescope goes here:
-		-- config_key = value,
-		mappings = {
-			i = {
-				-- map actions.which_key to <C-h> (default: <C-/>)
-				-- actions.which_key shows the mappings for your picker,
-				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
 
-				["<C-Down>"] = require("telescope.actions").cycle_history_next,
-				["<C-Up>"] = require("telescope.actions").cycle_history_prev,
-			},
-		},
-	},
-	pickers = {
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
-	},
-	extensions = {
-		-- Your extension configuration goes here:
-		-- extension_name = {
-		--   extension_config_key = value,
-		-- }
-		-- please take a look at the readme of the extension you want to configure
-		file_browser = {
-			theme = "ivy",
-			-- disables netrw and use telescope-file-browser in its place
-			hijack_netrw = true,
-			mappings = {
-				["i"] = {
-					-- your custom insert mode mappings
-				},
-				["n"] = {
-					-- your custom normal mode mappings
-				},
-			},
-		},
-	},
+        ["<C-Down>"] = require("telescope.actions").cycle_history_next,
+        ["<C-Up>"] = require("telescope.actions").cycle_history_prev,
+      },
+    },
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+    file_browser = {
+      theme = "ivy",
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        ["i"] = {
+          -- your custom insert mode mappings
+        },
+        ["n"] = {
+          -- your custom normal mode mappings
+        },
+      },
+    },
+  },
 })
 
 local utils = require("telescope.utils")
@@ -53,25 +53,30 @@ vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
 vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fR", function()
-	builtin.find_files({
-		cwd = utils.buffer_dir(),
-	})
+vim.keymap.set("n", "<leader>fh", function()
+  builtin.find_files({
+    hidden = true
+  })
+end, {})
+vim.keymap.set("n", "<leader>fr", function()
+  builtin.find_files({
+    cwd = utils.buffer_dir(),
+  })
 end, {})
 vim.keymap.set("n", "<leader>fo", function()
-	builtin.oldfiles({
-		only_cwd = true,
-	})
+  builtin.oldfiles({
+    only_cwd = true,
+  })
 end, {})
 vim.keymap.set("n", "<leader>fO", builtin.oldfiles, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 
 vim.api.nvim_set_keymap("n", "<leader>fe", ":Telescope file_browser<CR>", { noremap = true })
 vim.api.nvim_set_keymap(
-	"n",
-	"<space>fE",
-	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-	{ noremap = true }
+  "n",
+  "<space>fE",
+  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+  { noremap = true }
 )
 
 vim.keymap.set("n", "<leader>wg", builtin.live_grep, {})
